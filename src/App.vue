2cf1b5a2674ab27ef1014d5a51bf6fbc2f7ee381
irvanshandika <shandikamuhammadirvan@gmail>
2023-06-03 15:16:36 +0700

    Landing Page With Tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/navbar.vue'
+import Hero from './components/hero.vue'
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+
+  <main>
+    <Hero />
+  </main>
+</template>
+
+<style scoped></style>
